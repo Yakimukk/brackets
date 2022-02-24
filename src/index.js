5f3c1cfa9e8c53e95ad858dbe6097f014bed0c2d
Ekaterina Yakimuk <92802927+Yakimukk@users.noreply.github.com>
2022-02-24 20:32:13 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  arr = []
+  if (str.length % 2 != 0) {
+    return false;
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if ((bracketsConfig[j][0] === bracketsConfig[j][1])&&(str[i] === bracketsConfig[j][1])) {
+          if (stack[stack.length - 1] === str[i]) arr.pop()
+          else arr.push(str[i])
+        } else {
+              if (str[i] === bracketsConfig[j][0]) arr.push(str[i])
+              if ((str[i] === bracketsConfig[j][1])&&(arr[arr.length - 1] === bracketsConfig[j][0])) arr.pop()  
+        }
+      }
+    }
+  }
+  if (arr.length === 0) {
+    return true;
+  } else {
+    return false
+  }
 }
